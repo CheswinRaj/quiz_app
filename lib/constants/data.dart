1e9a565771ceef4208faa4064fb54b31ff9ef75f
Cheswin <ches422@gmail.com>
2024-11-13 21:51:51 +0530

    all done bit change

--- lib/constants/data.dart
+++ lib/constants/data.dart
@@ -27,302 +27,302 @@ List<QuestionModel> questions = [
     AnswerChoices: ["Vienna", "Zurich", "Berlin"],
 
   ),
-  // QuestionModel(
-  //   id: 5,
-  //   question: 'Which of the following is NOT a fruit?',
-  //   AnswerChoices: ["Rhubarb", "Tomatoes", "Avocados"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 6,
-  //   question: 'Where was the first example of paper money used?',
-  //   AnswerChoices: ["China", "Turkey", "Greece"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 7,
-  //   question: 'Who is generally considered the inventor of the motor car?',
-  //   AnswerChoices: ["Henry Ford", "Karl Benz", "Henry M. Leland"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 8,
-  //   question:
-  //   'If you were looking at Iguazu Falls, on what continent would you be?',
-  //   AnswerChoices: ["Asia", "Africa", "South America"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 9,
-  //   question:
-  //   'What number was the Apollo mission that successfully put a man on the moon for the first time in human history?',
-  //   AnswerChoices: ["Apollo 11", "Apollo 12", "Apollo 13"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 10,
-  //   question: 'Which of the following languages has the longest alphabet?',
-  //   AnswerChoices: ["Greek", "Russian", "Arabic"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 11,
-  //   question: 'Who was the lead singer of the band The Who?',
-  //   AnswerChoices: ["Roger Daltrey", "Don Henley", "Robert Plant"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 12,
-  //   question: 'What spirit is used in making a Tom Collins?',
-  //   AnswerChoices: ["Vodka", "Rum", "Gin"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 13,
-  //   question: 'The fear of insects is known as what?',
-  //   AnswerChoices: ["Entomophobia", "Arachnophobia", "Ailurophobia"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 14,
-  //   question:
-  //   'What was the name of the Franco-British supersonic commercial plane that operated from 1976-2003?',
-  //   AnswerChoices: ["Accord", "Concorde", "Mirage"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 15,
-  //   question: 'Which horoscope sign is a fish?',
-  //   AnswerChoices: ["Aquarius", "Cancer", "Pisces"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 16,
-  //   question: 'What is the largest US state (by landmass)?',
-  //   AnswerChoices: ["Texas", "Alaska", "California"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 17,
-  //   question: 'Which app has the most total users?',
-  //   AnswerChoices: ["TikTok", "Snapchat", "Instagram"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 18,
-  //   question: 'Which Game of Thrones character is known as the Young Wolf?',
-  //   AnswerChoices: ["Robb Stark", "Arya Stark", "Sansa Stark"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 19,
-  //   question: 'What city hosted the 2002 Olympic Games?',
-  //   AnswerChoices: ["Tokyo", "Beijing", "Sydney"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 20,
-  //   question:
-  //   'How many plays do people (generally) believe that Shakespeare wrote?',
-  //   AnswerChoices: ["27", "37", "47"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 21,
-  //   question:
-  //   'Which of the following was considered one of the Seven Ancient Wonders?',
-  //   AnswerChoices: ["Colosseum", "Great Wall of China", "Colossus of Rhodes"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 22,
-  //   question: 'Who directed the Academy Award-winning movie, Gladiator?',
-  //   AnswerChoices: ["Ridley Scott", "James Cameron", "Steven Soderbergh"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 23,
-  //   question: 'How long did dinosaurs live on the earth?',
-  //   AnswerChoices: [
-  //     "100-150 million years",
-  //     "150-200 million years",
-  //     "200+ million years"
-  //   ],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 24,
-  //   question: 'What Italian city is famous for its system of canals?',
-  //   AnswerChoices: ["Rome", "Naples", "Venice"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 25,
-  //   question: 'What is the strongest muscle in the human body?',
-  //   AnswerChoices: ["Jaw", "Heart", "Glutes"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 26,
-  //   question: 'What is the longest-running Broadway show ever?',
-  //   AnswerChoices: [
-  //     "Les Miserable",
-  //     "The Lion King",
-  //     "The Phantom of the Opera"
-  //   ],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 27,
-  //   question: 'Where was tea invented?',
-  //   AnswerChoices: ["England", "USA", "China"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 28,
-  //   question: 'Where was the earliest documented case of the Spanish flu?',
-  //   AnswerChoices: ["USA", "Spain", "Mexico"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 29,
-  //   question: 'Which of the following languages is NOT driven from Latin?',
-  //   AnswerChoices: ["French", "Portuguese", "English"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 30,
-  //   question:
-  //   'Arnold Schwarzenegger was married to a member of what famous US political family?',
-  //   AnswerChoices: ["The Kennedys", "The Bushes", "The Rockefellers"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 31,
-  //   question:
-  //   'Who sang the title song for the latest Bond film, No Time to Die?',
-  //   AnswerChoices: ["Adele", "Sam Smith", "Billie Eilish"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 32,
-  //   question:
-  //   'Which flies a green, white, and orange (in that order) tricolor flag? ',
-  //   AnswerChoices: ["Ireland", "Ivory Coast", "Italy"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 33,
-  //   question: 'What company makes the Xperia model of smartphone?',
-  //   AnswerChoices: ["Samsung", "Sony", "Nokia"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 34,
-  //   question: 'Which city is home to the Brandenburg Gate?',
-  //   AnswerChoices: ["Vienna", "Zurich", "Berlin"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 35,
-  //   question: 'Which of the following is NOT a fruit?',
-  //   AnswerChoices: ["Rhubarb", "Tomatoes", "Avocados"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 36,
-  //   question: 'Where was the first example of paper money used?',
-  //   AnswerChoices: ["China", "Turkey", "Greece"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 37,
-  //   question: 'Who is generally considered the inventor of the motor car?',
-  //   AnswerChoices: ["Henry Ford", "Karl Benz", "Henry M. Leland"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 38,
-  //   question:
-  //   'If you were looking at Iguazu Falls, on what continent would you be?',
-  //   AnswerChoices: ["Asia", "Africa", "South America"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 39,
-  //   question:
-  //   'What number was the Apollo mission that successfully put a man on the moon for the first time in human history?',
-  //   AnswerChoices: ["Apollo 11", "Apollo 12", "Apollo 13"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 40,
-  //   question: 'Which of the following languages has the longest alphabet?',
-  //   AnswerChoices: ["Greek", "Russian", "Arabic"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 41,
-  //   question: 'Who was the lead singer of the band The Who?',
-  //   AnswerChoices: ["Roger Daltrey", "Don Henley", "Robert Plant"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 42,
-  //   question: 'What spirit is used in making a Tom Collins?',
-  //   AnswerChoices: ["Vodka", "Rum", "Gin"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 43,
-  //   question: 'The fear of insects is known as what?',
-  //   AnswerChoices: ["Entomophobia", "Arachnophobia", "Ailurophobia"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 44,
-  //   question:
-  //   'What was the name of the Franco-British supersonic commercial plane that operated from 1976-2003?',
-  //   AnswerChoices: ["Accord", "Concorde", "Mirage"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 45,
-  //   question: 'Which horoscope sign is a fish?',
-  //   AnswerChoices: ["Aquarius", "Cancer", "Pisces"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 46,
-  //   question: 'What is the largest US state (by landmass)?',
-  //   AnswerChoices: ["Texas", "Alaska", "California"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 47,
-  //   question: 'Which app has the most total users?',
-  //   AnswerChoices: ["TikTok", "Snapchat", "Instagram"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 48,
-  //   question: 'Which Game of Thrones character is known as the Young Wolf?',
-  //   AnswerChoices: ["Robb Stark", "Arya Stark", "Sansa Stark"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 49,
-  //   question: 'What city hosted the 2002 Olympic Games?',
-  //   AnswerChoices: ["Tokyo", "Beijing", "Sydney"],
-  //
-  // ),
-  // QuestionModel(
-  //   id: 50,
-  //   question:
-  //   'How many plays do people (generally) believe that Shakespeare wrote?',
-  //   AnswerChoices: ["27", "37", "47"],
-  //
-  // ),
+  QuestionModel(
+    id: 5,
+    question: 'Which of the following is NOT a fruit?',
+    AnswerChoices: ["Rhubarb", "Tomatoes", "Avocados"],
+
+  ),
+  QuestionModel(
+    id: 6,
+    question: 'Where was the first example of paper money used?',
+    AnswerChoices: ["China", "Turkey", "Greece"],
+
+  ),
+  QuestionModel(
+    id: 7,
+    question: 'Who is generally considered the inventor of the motor car?',
+    AnswerChoices: ["Henry Ford", "Karl Benz", "Henry M. Leland"],
+
+  ),
+  QuestionModel(
+    id: 8,
+    question:
+    'If you were looking at Iguazu Falls, on what continent would you be?',
+    AnswerChoices: ["Asia", "Africa", "South America"],
+
+  ),
+  QuestionModel(
+    id: 9,
+    question:
+    'What number was the Apollo mission that successfully put a man on the moon for the first time in human history?',
+    AnswerChoices: ["Apollo 11", "Apollo 12", "Apollo 13"],
+
+  ),
+  QuestionModel(
+    id: 10,
+    question: 'Which of the following languages has the longest alphabet?',
+    AnswerChoices: ["Greek", "Russian", "Arabic"],
+
+  ),
+  QuestionModel(
+    id: 11,
+    question: 'Who was the lead singer of the band The Who?',
+    AnswerChoices: ["Roger Daltrey", "Don Henley", "Robert Plant"],
+
+  ),
+  QuestionModel(
+    id: 12,
+    question: 'What spirit is used in making a Tom Collins?',
+    AnswerChoices: ["Vodka", "Rum", "Gin"],
+
+  ),
+  QuestionModel(
+    id: 13,
+    question: 'The fear of insects is known as what?',
+    AnswerChoices: ["Entomophobia", "Arachnophobia", "Ailurophobia"],
+
+  ),
+  QuestionModel(
+    id: 14,
+    question:
+    'What was the name of the Franco-British supersonic commercial plane that operated from 1976-2003?',
+    AnswerChoices: ["Accord", "Concorde", "Mirage"],
+
+  ),
+  QuestionModel(
+    id: 15,
+    question: 'Which horoscope sign is a fish?',
+    AnswerChoices: ["Aquarius", "Cancer", "Pisces"],
+
+  ),
+  QuestionModel(
+    id: 16,
+    question: 'What is the largest US state (by landmass)?',
+    AnswerChoices: ["Texas", "Alaska", "California"],
+
+  ),
+  QuestionModel(
+    id: 17,
+    question: 'Which app has the most total users?',
+    AnswerChoices: ["TikTok", "Snapchat", "Instagram"],
+
+  ),
+  QuestionModel(
+    id: 18,
+    question: 'Which Game of Thrones character is known as the Young Wolf?',
+    AnswerChoices: ["Robb Stark", "Arya Stark", "Sansa Stark"],
+
+  ),
+  QuestionModel(
+    id: 19,
+    question: 'What city hosted the 2002 Olympic Games?',
+    AnswerChoices: ["Tokyo", "Beijing", "Sydney"],
+
+  ),
+  QuestionModel(
+    id: 20,
+    question:
+    'How many plays do people (generally) believe that Shakespeare wrote?',
+    AnswerChoices: ["27", "37", "47"],
+
+  ),
+  QuestionModel(
+    id: 21,
+    question:
+    'Which of the following was considered one of the Seven Ancient Wonders?',
+    AnswerChoices: ["Colosseum", "Great Wall of China", "Colossus of Rhodes"],
+
+  ),
+  QuestionModel(
+    id: 22,
+    question: 'Who directed the Academy Award-winning movie, Gladiator?',
+    AnswerChoices: ["Ridley Scott", "James Cameron", "Steven Soderbergh"],
+
+  ),
+  QuestionModel(
+    id: 23,
+    question: 'How long did dinosaurs live on the earth?',
+    AnswerChoices: [
+      "100-150 million years",
+      "150-200 million years",
+      "200+ million years"
+    ],
+
+  ),
+  QuestionModel(
+    id: 24,
+    question: 'What Italian city is famous for its system of canals?',
+    AnswerChoices: ["Rome", "Naples", "Venice"],
+
+  ),
+  QuestionModel(
+    id: 25,
+    question: 'What is the strongest muscle in the human body?',
+    AnswerChoices: ["Jaw", "Heart", "Glutes"],
+
+  ),
+  QuestionModel(
+    id: 26,
+    question: 'What is the longest-running Broadway show ever?',
+    AnswerChoices: [
+      "Les Miserable",
+      "The Lion King",
+      "The Phantom of the Opera"
+    ],
+
+  ),
+  QuestionModel(
+    id: 27,
+    question: 'Where was tea invented?',
+    AnswerChoices: ["England", "USA", "China"],
+
+  ),
+  QuestionModel(
+    id: 28,
+    question: 'Where was the earliest documented case of the Spanish flu?',
+    AnswerChoices: ["USA", "Spain", "Mexico"],
+
+  ),
+  QuestionModel(
+    id: 29,
+    question: 'Which of the following languages is NOT driven from Latin?',
+    AnswerChoices: ["French", "Portuguese", "English"],
+
+  ),
+  QuestionModel(
+    id: 30,
+    question:
+    'Arnold Schwarzenegger was married to a member of what famous US political family?',
+    AnswerChoices: ["The Kennedys", "The Bushes", "The Rockefellers"],
+
+  ),
+  QuestionModel(
+    id: 31,
+    question:
+    'Who sang the title song for the latest Bond film, No Time to Die?',
+    AnswerChoices: ["Adele", "Sam Smith", "Billie Eilish"],
+
+  ),
+  QuestionModel(
+    id: 32,
+    question:
+    'Which flies a green, white, and orange (in that order) tricolor flag? ',
+    AnswerChoices: ["Ireland", "Ivory Coast", "Italy"],
+
+  ),
+  QuestionModel(
+    id: 33,
+    question: 'What company makes the Xperia model of smartphone?',
+    AnswerChoices: ["Samsung", "Sony", "Nokia"],
+
+  ),
+  QuestionModel(
+    id: 34,
+    question: 'Which city is home to the Brandenburg Gate?',
+    AnswerChoices: ["Vienna", "Zurich", "Berlin"],
+
+  ),
+  QuestionModel(
+    id: 35,
+    question: 'Which of the following is NOT a fruit?',
+    AnswerChoices: ["Rhubarb", "Tomatoes", "Avocados"],
+
+  ),
+  QuestionModel(
+    id: 36,
+    question: 'Where was the first example of paper money used?',
+    AnswerChoices: ["China", "Turkey", "Greece"],
+
+  ),
+  QuestionModel(
+    id: 37,
+    question: 'Who is generally considered the inventor of the motor car?',
+    AnswerChoices: ["Henry Ford", "Karl Benz", "Henry M. Leland"],
+
+  ),
+  QuestionModel(
+    id: 38,
+    question:
+    'If you were looking at Iguazu Falls, on what continent would you be?',
+    AnswerChoices: ["Asia", "Africa", "South America"],
+
+  ),
+  QuestionModel(
+    id: 39,
+    question:
+    'What number was the Apollo mission that successfully put a man on the moon for the first time in human history?',
+    AnswerChoices: ["Apollo 11", "Apollo 12", "Apollo 13"],
+
+  ),
+  QuestionModel(
+    id: 40,
+    question: 'Which of the following languages has the longest alphabet?',
+    AnswerChoices: ["Greek", "Russian", "Arabic"],
+
+  ),
+  QuestionModel(
+    id: 41,
+    question: 'Who was the lead singer of the band The Who?',
+    AnswerChoices: ["Roger Daltrey", "Don Henley", "Robert Plant"],
+
+  ),
+  QuestionModel(
+    id: 42,
+    question: 'What spirit is used in making a Tom Collins?',
+    AnswerChoices: ["Vodka", "Rum", "Gin"],
+
+  ),
+  QuestionModel(
+    id: 43,
+    question: 'The fear of insects is known as what?',
+    AnswerChoices: ["Entomophobia", "Arachnophobia", "Ailurophobia"],
+
+  ),
+  QuestionModel(
+    id: 44,
+    question:
+    'What was the name of the Franco-British supersonic commercial plane that operated from 1976-2003?',
+    AnswerChoices: ["Accord", "Concorde", "Mirage"],
+
+  ),
+  QuestionModel(
+    id: 45,
+    question: 'Which horoscope sign is a fish?',
+    AnswerChoices: ["Aquarius", "Cancer", "Pisces"],
+
+  ),
+  QuestionModel(
+    id: 46,
+    question: 'What is the largest US state (by landmass)?',
+    AnswerChoices: ["Texas", "Alaska", "California"],
+
+  ),
+  QuestionModel(
+    id: 47,
+    question: 'Which app has the most total users?',
+    AnswerChoices: ["TikTok", "Snapchat", "Instagram"],
+
+  ),
+  QuestionModel(
+    id: 48,
+    question: 'Which Game of Thrones character is known as the Young Wolf?',
+    AnswerChoices: ["Robb Stark", "Arya Stark", "Sansa Stark"],
+
+  ),
+  QuestionModel(
+    id: 49,
+    question: 'What city hosted the 2002 Olympic Games?',
+    AnswerChoices: ["Tokyo", "Beijing", "Sydney"],
+
+  ),
+  QuestionModel(
+    id: 50,
+    question:
+    'How many plays do people (generally) believe that Shakespeare wrote?',
+    AnswerChoices: ["27", "37", "47"],
+
+  ),
 ];
 List<Anserchoicemodel> answers = [
   Anserchoicemodel(id: 1, questionID: 1, Choice: 2),
